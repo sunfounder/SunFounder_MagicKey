a719eec37675feb037a0d95171065f83f82da28d
Dream <survice@sunfounder.com>
2017-03-04 10:03:10 +0800

    Add drum note map, optimize midi code

--- multi-controller-midi/multi-controller-midi.ino
+++ multi-controller-midi/multi-controller-midi.ino
@@ -2,206 +2,183 @@
 #include "notemap.h"
 
 #define MINTOUCH 900
-int currentVelocity = 110;
+#define CHANNEL_KEYBOARD 0
+#define CHANNEL_DRUM 9  // CHANNEL
+#define CONTROL_VOLUME  7
 
+int currentVelocity = 127;
+int pmmin;
+int pmmax;
+
+int mod = 0;
+int channel = CHANNEL_KEYBOARD;
+
+const int selector         = 7;
+const int channelPlus      = 3;
+const int channelMinus     = 2;
+int statusSelcetor         = 0;
 //==============================================
 // Set variables
-// valueAX 直接读取到的模拟值
-// statusAX 模拟值通过门限转成数字量
-// lastStatusAX 读值有改变才进行输出
-int valueA0  = 0;
-int valueA1  = 0;
-int valueA2  = 0;
-int valueA3  = 0;
-int valueA4  = 0;
-int valueA5  = 0;
-int valueA6  = 0;
-int valueA7  = 0;
-int valueA8  = 0;
-int valueA9  = 0;
-int valueA10 = 0;
-int valueA11 = 0;
+// holeAX[0] 直接读取到的模拟值    valueAX
+// holeAX[1] 模拟值转成数字量      statusAX
+// holeAX[2] 读值有改变才进行输出  lastStatusAX
+// holeAX[3] note map              NOTE_A4
+// holeAX[4] lastNoteAX, 变调的值  lastNoteAX
+int holeA2[]  = {0, 0, 0, 0};
+int holeA3[]  = {0, 0, 0, 0};
+int holeA4[]  = {0, 0, 0, NOTE_A4, 0};
+int holeA5[]  = {0, 0, 0, NOTE_A5, 0};
+int holeA6[]  = {0, 0, 0, NOTE_A6, 0};
+int holeA7[]  = {0, 0, 0, NOTE_A7, 0};
+int holeA8[]  = {0, 0, 0, NOTE_A8, 0};
+int holeA9[]  = {0, 0, 0, NOTE_A9, 0};
+int holeA10[] = {0, 0, 0, NOTE_A10, 0};
+int holeA11[] = {0, 0, 0, NOTE_A11, 0};
 
-int statusA0  = 0;
-int statusA1  = 0;
-int statusA2  = 0;
-int statusA3  = 0;
-int statusA4  = 0;
-int statusA5  = 0;
-int statusA6  = 0;
-int statusA7  = 0;
-int statusA8  = 0;
-int statusA9  = 0;
-int statusA10 = 0;
-int statusA11 = 0;
-
-int lastStatusA0  = 0;
-int lastStatusA1  = 0;
-int lastStatusA2  = 0;
-int lastStatusA3  = 0;
-int lastStatusA4  = 0;
-int lastStatusA5  = 0;
-int lastStatusA6  = 0;
-int lastStatusA7  = 0;
-int lastStatusA8  = 0;
-int lastStatusA9  = 0;
-int lastStatusA10 = 0;
-int lastStatusA11 = 0;
 //==============================================
 
 void setup() {
   Serial.begin(115200);
+  pinMode(channelPlus, INPUT_PULLUP);
+  pinMode(channelMinus, INPUT_PULLUP);
+  controlChange(channel, CONTROL_VOLUME, 127);
 }
 
 void readStatus(){
-  valueA0  = analogRead(A0);
-  valueA1  = analogRead(A1);
-  valueA2  = analogRead(A2);
-  valueA3  = analogRead(A3);
-  valueA4  = analogRead(A4);
-  valueA5  = analogRead(A5);
-  valueA6  = analogRead(A6);
-  valueA7  = analogRead(A7);
-  valueA8  = analogRead(A8);
-  valueA9  = analogRead(A9);
-  valueA10 = analogRead(A10);
-  valueA11 = analogRead(A11);
+  //statusSelcetor = digitalRead(selector);
+  //if(statusSelcetor == 0){    // hole 开关
+    holeA2[0]  = analogRead(A2);
+    holeA3[0]  = analogRead(A3);
+    holeA4[0]  = analogRead(A4);
+    holeA5[0]  = analogRead(A5);
+    holeA6[0]  = analogRead(A6);
+    holeA7[0]  = analogRead(A7);
+    holeA8[0]  = analogRead(A8);
+    holeA9[0]  = analogRead(A9);
+    holeA10[0] = analogRead(A10);
+    holeA11[0] = analogRead(A11);
 
-  //Serial.print("A10: ");Serial.print(valueA10);
- //Serial.print("    A11: ");Serial.println(valueA11);
+    if (holeA2[0] < MINTOUCH)    holeA2[1]   = 0;
+    else                      holeA2[1]   = 1;
+    if (holeA3[0] < MINTOUCH)    holeA3[1]   = 0;
+    else                      holeA3[1]   = 1;
+    if (holeA4[0] < MINTOUCH)    holeA4[1]   = 0;
+    else                      holeA4[1]   = 1;
+    if (holeA5[0] < MINTOUCH)    holeA5[1]   = 0;
+    else                      holeA5[1]   = 1;
+    if (holeA6[0] < MINTOUCH)    holeA6[1]   = 0;
+    else                      holeA6[1]   = 1;
+    if (holeA7[0] < MINTOUCH)    holeA7[1]   = 0;
+    else                      holeA7[1]   = 1;
+    if (holeA8[0] < MINTOUCH)    holeA8[1]   = 0;
+    else                      holeA8[1]   = 1;
+    if (holeA9[0] < MINTOUCH)    holeA9[1]   = 0;
+    else                      holeA9[1]   = 1;
+    if (holeA10[0] < MINTOUCH)   holeA10[1]  = 0;
+    else                      holeA10[1]  = 1;
+    if (holeA11[0] < MINTOUCH)   holeA11[1]  = 0;
+    else                      holeA11[1]  = 1;
+/*  // 音量可调节
+    //currentVelocity = map(valueA0, 0, 1024, 0, 127);
+    int ec1 = map(valueA3, pmmin, pmmax, 0, 127);
+    //Serial.print("pm :");Serial.println(valueA3);
+    /*if(valueA1 < 1000){
+      controlChange(0, 7, ec1);
+    }
+*/
 
-  Serial.print(valueA4);Serial.print(',');
-  Serial.print(valueA5);Serial.print(',');
-  Serial.print(valueA6);Serial.print(',');
-  Serial.print(valueA7);Serial.print(',');
-  Serial.print(valueA8);Serial.print(',');
-  Serial.print(valueA9);Serial.print(',');
-  Serial.print(valueA10);Serial.print(',');
-  Serial.print(valueA11);Serial.print(',');
-
-  if (valueA4 < MINTOUCH)    statusA4   = 0;
-  else                      statusA4   = 1;
-  if (valueA5 < MINTOUCH)    statusA5   = 0;
-  else                      statusA5   = 1;
-  if (valueA6 < MINTOUCH)    statusA6   = 0;
-  else                      statusA6   = 1;
-  if (valueA7 < MINTOUCH)    statusA7   = 0;
-  else                      statusA7   = 1;
-  if (valueA8 < MINTOUCH)    statusA8   = 0;
-  else                      statusA8   = 1;
-  if (valueA9 < MINTOUCH)    statusA9   = 0;
-  else                      statusA9   = 1;
-  if (valueA10 < MINTOUCH)   statusA10  = 0;
-  else                      statusA10  = 1;
-  if (valueA11 < MINTOUCH)   statusA11  = 0;
-  else                      statusA11  = 1;
-
-  //currentVelocity = map(valueA0, 0, 1024, 0, 127);
-  int ec1 = map(valueA1, 0, 1024, 0, 127);
-  controlChange(0, 13, ec1);
+/*  // 串口绘图器
+    Serial.print(holeA3[0]);Serial.print(',');
+    Serial.print(holeA4[0]);Serial.print(',');
+    Serial.print(holeA5[0]);Serial.print(',');
+    Serial.print(holeA6[0]);Serial.print(',');
+    Serial.print(holeA7[0]);Serial.print(',');
+    Serial.print(holeA8[0]);Serial.print(',');
+    Serial.print(holeA9[0]);Serial.print(',');
+    Serial.print(holeA10[0]);Serial.print(',');
+    Serial.print(holeA11[0]);Serial.print(',');
+*/ //}
 }
 
-void noteHandle(){
+void holeHandle(int *holeAX){
+  if(holeAX[2] != holeAX[1]){
+    if(holeAX[1] == 0){
+      noteOn(channel, (holeAX[3] + mod), currentVelocity);
+      holeAX[4] = holeAX[3] + mod;
+      Serial.print("noteOn  ");Serial.println(holeAX[3] + mod);
+    }
+    else{
+      noteOff(channel, holeAX[4], currentVelocity);
+      Serial.print("      noteOff  ");Serial.println(holeAX[4]);
+    }
+    holeAX[2] = holeAX[1];
+  }
+  //delay(20);
+}
+
+void scan(){
+  int a = digitalRead(channelPlus);
+  int b = digitalRead(channelMinus);
+  if(a == 0){
+    while(a == 0){a = digitalRead(channelPlus);}
+    if(a == 1){
+        channel += 1;
+        if(channel > 15){
+            channel = 15;
+        }
+    }
+  }
+
+  if(b == 0){
+    while(b == 0){b = digitalRead(channelMinus);}
+    if(b == 1){
+        channel -= 1;
+        if(channel < 0){
+            channel = 0;
+        }
+    }
+  }
+  if((holeA2[1] + holeA3[1]) == 0) // A2和A3同时按下，升1个调
+    mod = 1;
+  else if (holeA2[1] == 0)  // 仅A2按下，升一个八度
+    mod = 12;
+  else                      // 仅A3按下，降低一个八度
+    mod = -12;
+
   // A4
-  if (lastStatusA4 != statusA4) {
-    if (statusA4 == 0) {
-      noteOn(0, NOTE_A4, currentVelocity);
-      //a4 = NOTE_A4;
-    }
-    else{
-      noteOff(0, NOTE_A4, currentVelocity);
-    }
-    lastStatusA4 = statusA4;
-  }
+  holeHandle(holeA4);
   // A5
-  if (lastStatusA5 != statusA5) {
-    if (statusA5 == 0) {
-      noteOn(0, NOTE_A5, currentVelocity);
-      //a5 = NOTE_A5;
-    }
-    else{
-      noteOff(0, NOTE_A5, currentVelocity);
-    }
-    lastStatusA5 = statusA5;
-  }
+  holeHandle(holeA5);
   // A6
-  if (lastStatusA6 != statusA6) {
-    if (statusA6 == 0) {
-      noteOn(0, NOTE_A6, currentVelocity);
-    }
-    else{
-      noteOff(0, NOTE_A6, currentVelocity);
-    }
-    lastStatusA6 = statusA6;
-  }
+  holeHandle(holeA6);
   // A7
-  if (lastStatusA7 != statusA7) {
-    if (statusA7 == 0) {
-      noteOn(0, NOTE_A7, currentVelocity);
-    }
-    else{
-      noteOff(0, NOTE_A7, currentVelocity);
-    }
-    lastStatusA7 = statusA7;
-  }
+  holeHandle(holeA7);
   // A8
-  if (lastStatusA8 != statusA8) {
-    if (statusA8 == 0) {
-      noteOn(0, NOTE_A8, currentVelocity);
-    }
-    else{
-      noteOff(0, NOTE_A8, currentVelocity);
-    }
-    lastStatusA8 = statusA8;
-  }
+  holeHandle(holeA8);
   // A9
-  if (lastStatusA9 != statusA9) {
-    if (statusA9 == 0) {
-      noteOn(0, NOTE_A9, currentVelocity);
-    }
-    else{
-      noteOff(0, NOTE_A9, currentVelocity);
-    }
-    lastStatusA9 = statusA9;
-  }
+  holeHandle(holeA9);
   // A10
-  if (lastStatusA10 != statusA10) {
-    if (statusA10 == 0) {
-      noteOn(0, NOTE_A10, currentVelocity);
-    }
-    else{
-      noteOff(0, NOTE_A10, currentVelocity);
-    }
-    lastStatusA10 = statusA10;
-  }
+  holeHandle(holeA10);
   // A11
-  if (lastStatusA11 != statusA11) {
-    if (statusA11 == 0) {
-      noteOn(0, NOTE_A11, currentVelocity);
-    }
-    else{
-      noteOff(0, NOTE_A11, currentVelocity);
-    }
-    lastStatusA11 = statusA11;
-  }
-  delay(20);
+  holeHandle(holeA11);
 }
 
 void loop() {
+  readStatus();
+  scan();
+
   Serial.print(0);Serial.print(',');
   Serial.print(MINTOUCH);Serial.print(',');
-  readStatus();
-  noteHandle();
   Serial.println(1023);
 }
 
-void noteOn(byte channel, byte pitch, byte velocity) {
-  midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
+void noteOn(byte chn, byte pitch, byte velocity) {
+  midiEventPacket_t noteOn = {0x09, 0x90 | chn, pitch, velocity};
   MidiUSB.sendMIDI(noteOn);
   MidiUSB.flush();
 }
-void noteOff(byte channel, byte pitch, byte velocity) {
-  midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
+void noteOff(byte chn, byte pitch, byte velocity) {
+  midiEventPacket_t noteOff = {0x08, 0x80 | chn, pitch, velocity};
   MidiUSB.sendMIDI(noteOff);
   MidiUSB.flush();
 }
@@ -210,8 +187,8 @@ void noteOff(byte channel, byte pitch, byte velocity) {
 // Second parameter is the event type, combined with the channel.
 // Third parameter is the control number number (0-119).
 // Fourth parameter is the control value (0-127).
-void controlChange(byte channel, byte control, byte value) {
-  midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
+void controlChange(byte chn, byte ctrl, byte value) {
+  midiEventPacket_t event = {0x0B, 0xB0 | chn, ctrl, value};
   MidiUSB.sendMIDI(event);
   MidiUSB.flush();
 }
--- multi-controller-midi/notemap.h
+++ multi-controller-midi/notemap.h
@@ -13,16 +13,25 @@
 // |   8    | 96  | 97  | 98  | 99  | 100 | 101 | 102 | 103 | 104 | 105 | 106 | 107 |
 // |   9    | 108 | 109 | 110 | 111 | 112 | 113 | 114 | 115 | 116 | 117 | 118 | 119 |
 // |   10   | 120 | 121 | 122 | 123 | 124 | 125 | 126 | 127 |     |     |     |     |
-
-#define NOTE_A0
-#define NOTE_A1
-#define NOTE_A2
-#define NOTE_A3
-#define NOTE_A4   48   // C4
-#define NOTE_A5   50   // D4
-#define NOTE_A6   52   // E4
-#define NOTE_A7   53   // F4
-#define NOTE_A8   55   // G4
-#define NOTE_A9   57   // A4
-#define NOTE_A10  59   // B4
-#define NOTE_A11  60   // C5
+///*
+// keyboard note
+#define NOTE_A4   60   // C5
+#define NOTE_A5   62   // D5
+#define NOTE_A6   64   // E5
+#define NOTE_A7   65   // F5
+#define NOTE_A8   67   // G5
+#define NOTE_A9   69   // A5
+#define NOTE_A10  71   // B5
+#define NOTE_A11  72   // C6
+//*/
+/*
+// drum note
+#define NOTE_A4   52   // C5
+#define NOTE_A5   53   // D5
+#define NOTE_A6   54   // E5
+#define NOTE_A7   55   // F5
+#define NOTE_A8   56   // G5
+#define NOTE_A9   57   // A5
+#define NOTE_A10  58   // B5
+#define NOTE_A11  59   // C6
+*/
